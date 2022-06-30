055407e70988e94c1818a310c506b54ed6f7f3ef
Venkata Kalyan Sagi <kalyansagi.555@gmail.com>
2022-06-29 20:19:38 -0400

    Working model

--- src/ConvNet.py
+++ src/ConvNet.py
@@ -20,26 +20,45 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Step 1 - Building the Convolution Layer
-classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Convolution2D(32, 3, 3, input_shape=(64, 64, 3), activation='relu'))
 
 # Step 2 - Building the Pooling Layer
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Adding The Second Convolutional Layer
-classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Convolution2D(32, 3, 3, activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Step 3 - Building the Flattening Layer
 classifier.add(Flatten())
 
 # Step 4 - Building the Fully Connected Layer
-classifier.add(Dense(output_dim = 128, activation = 'relu'))
-classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+classifier.add(Dense(units=128, activation='relu'))
+classifier.add(Dense(units=1, activation='sigmoid'))
+
 
 # Compiling the ConvNet
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Part 2 - Fitting the ConvNet to the Images
 
 from keras.preprocessing.image import ImageDataGenerator
-# ..... Fill the Rest (a Few Lines of Code!)
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
+training_set = train_datagen.flow_from_directory('training_set',
+                                                 target_size=(64, 64),
+                                                 batch_size=32,
+                                                 class_mode='binary')
+test_set = test_datagen.flow_from_directory('test_set',
+                                            target_size=(64, 64),
+                                            batch_size=32,
+                                            class_mode='binary')
+classifier.fit_generator(training_set,
+                    steps_per_epoch=8000,
+                    epochs=25,
+                    validation_data=test_set,
+                    validation_steps=2000)
